333e5c7eac685d3cff6c184ab22b298bd191128c
Philldanupe <112665300+Philldanupe@users.noreply.github.com>
2023-02-05 23:15:49 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,52 @@
+#---root/main.tf---
+resource "aws_ecs_cluster" "cluster" {
+  name = "ecs-cluster"
+
+  capacity_providers = ["FARGATE_SPOT", "FARGATE"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+  }
+
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
+}
+
+module "ecs-fargate" {
+  source  = "umotif-public/ecs-fargate/aws"
+  version = "~> 6.1.0"
+
+  name_prefix        = "ecs-container"
+  vpc_id             = "vpc-0dbaefd43c5957a46"
+  private_subnet_ids = ["subnet-034ac6bbc4cf98849"]
+
+  cluster_id = aws_ecs_cluster.cluster.id
+
+  task_container_image   = "centos"
+  task_definition_cpu    = 256
+  task_definition_memory = 512
+
+  task_container_port             = 80
+  task_container_assign_public_ip = true
+
+  load_balanced = false
+
+  target_groups = [
+    {
+      target_group_name = "wk23-tg-fargate-example"
+      container_port    = 80
+    }
+  ]
+
+  health_check = {
+    port = "traffic-port"
+    path = "/"
+  }
+
+  tags = {
+    Environment = "test"
+    Project     = "Test"
+  }
+}
